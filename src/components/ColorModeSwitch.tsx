0c502473d18bb274583dce7f15686f344a6cba21
ma-shamshiri <ma.shamshiri@gmail.com>
2024-03-31 14:28:28 -0400

    Modify ColorModeSwitcher.

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,35 +1,55 @@
 import { useColorMode, Button, useBreakpointValue } from "@chakra-ui/react";
 import { SunIcon } from "@chakra-ui/icons";
 import { LuMoonStar } from "react-icons/lu";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const ColorModeSwitch = () => {
+  const [boxLoaded, setBoxLoaded] = useState(false);
+
+  const handleBoxLoad = () => {
+    setBoxLoaded(true);
+  };
+
   const { toggleColorMode, colorMode } = useColorMode();
 
   const isLightMode = colorMode === "light";
 
-  const iconSize = useBreakpointValue({ base: "2rem", lg: "2rem" });
-  const iconContainerSize = useBreakpointValue({ base: "6rem", lg: "6rem" });
+  const iconSize = useBreakpointValue({ base: "1.3rem", lg: "1.5rem" });
+  const iconContainerSize = useBreakpointValue({
+    base: "3.1rem",
+    lg: "3.5rem",
+  });
+
+  useEffect(() => {}, [boxLoaded]);
 
   return (
-    <Button
-      onClick={toggleColorMode}
-      borderRadius="full"
-      p={1}
-      transition="background 0.4s, color 0.3s"
-      _hover={{
-        bg: isLightMode ? "#2220ba" : "gold",
-        color: isLightMode ? "white" : "black",
-      }}
-      bg={isLightMode ? "gray.300" : "gray.600"}
-      color={isLightMode ? "black" : "white"}
-      boxSize={iconContainerSize}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+      onAnimationComplete={handleBoxLoad}
     >
-      {isLightMode ? (
-        <LuMoonStar size={iconSize} />
-      ) : (
-        <SunIcon boxSize={iconSize} />
-      )}
-    </Button>
+      <Button
+        onClick={toggleColorMode}
+        borderRadius="full"
+        p={1}
+        transition="background 0.4s, color 0.3s"
+        _hover={{
+          bg: isLightMode ? "#2220ba" : "gold",
+          color: isLightMode ? "white" : "black",
+        }}
+        bg={isLightMode ? "gray.300" : "gray.600"}
+        color={isLightMode ? "black" : "white"}
+        boxSize={iconContainerSize}
+      >
+        {isLightMode ? (
+          <LuMoonStar size={iconSize} />
+        ) : (
+          <SunIcon boxSize={iconSize} />
+        )}
+      </Button>
+    </motion.div>
   );
 };
 
